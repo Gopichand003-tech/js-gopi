22e1e7eae2ddec1270913f0aaec6208e74b27709
Gopichand003-tech <gopichandchennapalli@gmail.com>
2025-01-19 12:52:18 +0530

    Add files via upload

---
+++ conversions.js
@@ -0,0 +1,16 @@
+let islogin = ""
+let booleanisloggedin = Boolean(islogin)
+console.log(booleanisloggedin)
+// true => 1 , false => 0
+// "1" => true , false => ""
+
+let score = true
+console.log(typeof score)
+
+let valueinnumber = Number(score)
+console.log( valueinnumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true = 1 , false = 0
+
---
+++ datatypes.js
@@ -0,0 +1,14 @@
+/*
+bool = true/false
+string = ""
+bigint
+number = 2 to power 53
+null = standalone value
+symbol=>unique
+
+
+*/
+ let age = 12
+let name = "gopichand"
+console.log(typeof age)
+console.log(name, typeof "gopichand")
